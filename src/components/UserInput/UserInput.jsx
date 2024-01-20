0929d96136b5e7274ab342dfbb9c335e1ce33e5b
Keiichi Sato <51529803+kei1-st@users.noreply.github.com>
2024-01-20 05:38:58 +0000

    add input-group

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,9 +1,28 @@
-import "./UserInput.css"
+import './UserInput.css';
 
 export default function UserInput() {
   return (
     <div id="user-input">
-      <p>test</p>
+      <div class="input-group">
+        <div>
+          <label>INITIAL INVESTMENT</label>
+          <input />
+        </div>
+        <div>
+          <label>ANNUAL INVESTMENT</label>
+          <input />
+        </div>
+      </div>
+      <div class="input-group">
+        <div>
+          <label>EXPECTED RETURN</label>
+          <input />
+        </div>
+        <div>
+          <label>DURATION</label>
+          <input />
+        </div>
+      </div>
     </div>
-  )
+  );
 }

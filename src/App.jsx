b89111ca8671c53816db7c0390ab1745976c916a
Keiichi Sato <51529803+kei1-st@users.noreply.github.com>
2024-01-20 07:46:43 +0000

    add input value handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import Header from './components/Header/Header.jsx';
 import InputScreen from './components/UserInput/UserInput.jsx';
 import Result from './components/Result/Result.jsx';
+import { useState } from 'react';
 
 function App() {
+  const [initialInvestment, setInitialInvestment] = useState(0);
+  const [annualInvestment, setAnnualInvestment] = useState(0);
+  const [expectedReturn, setExpectedReturn] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  function setInitialInvestmentValue(e) {
+    setInitialInvestment(e.target.value);
+  }
+  function setAnnualInvestmentValue(e) {
+    setAnnualInvestment(e.target.value);
+  }
+  function setExpectedReturnValue(e) {
+    setExpectedReturn(e.target.value);
+  }
+  function setDurationValue(e) {
+    setDuration(e.target.value);
+  }
+
   return (
     <>
       <Header />
-      <InputScreen />
+      <InputScreen
+        initialInvestment={initialInvestment}
+        annualInvestment={annualInvestment}
+        expectedReturn={expectedReturn}
+        duration={duration}
+        setInitialInvestmentValue={setInitialInvestmentValue}
+        setAnnualInvestmentValue={setAnnualInvestmentValue}
+        setExpectedReturnValue={setExpectedReturnValue}
+        setDurationValue={setDurationValue}
+      />
       <Result />
     </>
   );
--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,26 +1,40 @@
 import './UserInput.css';
+import { useState } from 'react';
 
-export default function UserInput() {
+export default function UserInput({
+  initialInvestment,
+  annualInvestment,
+  expectedReturn,
+  duration,
+  setInitialInvestmentValue,
+  setAnnualInvestmentValue,
+  setExpectedReturnValue,
+  setDurationValue,
+}) {
   return (
     <div id="user-input">
       <div class="input-group">
         <div>
           <label>INITIAL INVESTMENT</label>
-          <input type="number"/>
+          <input type="number" val={initialInvestment} onChange={setInitialInvestmentValue} />
+          {console.log(initialInvestment)}
         </div>
         <div>
           <label>ANNUAL INVESTMENT</label>
-          <input type="number"/>
+          <input type="number" val={annualInvestment} onChange={setAnnualInvestmentValue} />
+          {console.log(annualInvestment)}
         </div>
       </div>
       <div class="input-group">
         <div>
           <label>EXPECTED RETURN</label>
-          <input type="number"/>
+          <input type="number" val={expectedReturn} onChange={setExpectedReturnValue} />
+          {console.log(expectedReturn)}
         </div>
         <div>
           <label>DURATION</label>
-          <input type="number"/>
+          <input type="number" cal={duration} onChange={setDurationValue} />
+          {console.log(duration)}
         </div>
       </div>
     </div>

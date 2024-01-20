92388808761b9a911af2e95df5ce54c2ddc75a45
Keiichi Sato <51529803+kei1-st@users.noreply.github.com>
2024-01-20 05:12:01 +0000

    add UserInput()

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import Header from "./components/Header/Header.jsx";
+import Header from './components/Header/Header.jsx';
+import InputScreen from './components/UserInput/UserInput.jsx';
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <InputScreen />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/UserInput/UserInput.jsx
@@ -0,0 +1,9 @@
+import "./UserInput.css"
+
+export default function UserInput() {
+  return (
+    <div id="user-input">
+      <p>test</p>
+    </div>
+  )
+}
